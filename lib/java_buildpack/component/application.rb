b41575827f3af085a9c58599c1b5c0acb45907c8
alokhm <akshukla226@gmail.com>
2015-03-06 14:58:41 +0530

    Update application.rb

--- lib/java_buildpack/component/application.rb
+++ lib/java_buildpack/component/application.rb
@@ -57,7 +57,6 @@ module JavaBuildpack
         @environment = ENV.to_hash
         @details     = parse(@environment.delete('VCAP_APPLICATION'))
         @services    = Services.new(parse(@environment.delete('VCAP_SERVICES')))
-        puts  @environment
       end
 
       private
