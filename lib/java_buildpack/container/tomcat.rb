3edf350cd23263c1021b7c71a6ec922260b2b5e2
alokhm <akshukla226@gmail.com>
2015-04-22 17:42:27 +0530

    Update tomcat.rb

--- lib/java_buildpack/container/tomcat.rb
+++ lib/java_buildpack/container/tomcat.rb
@@ -48,7 +48,7 @@ module JavaBuildpack
       # (see JavaBuildpack::Component::ModularComponent#sub_components)
       def sub_components(context)
         [
-          TomcatInstance.new(sub_configuration_context(context, 'tomcat7')),
+          TomcatInstance.new(sub_configuration_context(context, 'tomcat6')),
           TomcatLifecycleSupport.new(sub_configuration_context(context, 'lifecycle_support')),
           YamlParser.new(context),
           TomcatLoggingSupport.new(sub_configuration_context(context, 'logging_support')),
