d6de7987e0c47c19950febedb68de10d624ec10e
alokhm <akshukla226@gmail.com>
2015-04-28 01:19:28 +0530

    Update tomcat.rb

--- lib/java_buildpack/container/tomcat.rb
+++ lib/java_buildpack/container/tomcat.rb
@@ -62,7 +62,7 @@ module JavaBuildpack
                              end
         [
           
-          TomcatInstance.new(sub_configuration_context(context, "#{@config["servername"]}")),
+          TomcatInstance.new(sub_configuration_context(context, 'tomcat7')),
           TomcatLifecycleSupport.new(sub_configuration_context(context, 'lifecycle_support')),
           YamlParser.new(context),
           TomcatLoggingSupport.new(sub_configuration_context(context, 'logging_support')),
