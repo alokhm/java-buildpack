944212f5c624252a4189d9bae6f2acd770f5a2e0
alokhm <akshukla226@gmail.com>
2015-04-29 14:33:39 +0530

    Update open_jdk_like.rb

--- lib/java_buildpack/jre/open_jdk_like.rb
+++ lib/java_buildpack/jre/open_jdk_like.rb
@@ -64,7 +64,7 @@ module JavaBuildpack
       end
       
       def find_jdk_version_config
-         @configuration=@configuration[$appname]
+         @configuration=@configuration['$appname']
       end 
       private
 
