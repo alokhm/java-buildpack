7b04fd18248a92640f2337c97b34d56859d6b284
alokhm <akshukla226@gmail.com>
2015-05-07 15:04:05 +0530

    Update jce_support.rb

--- lib/java_buildpack/framework/jce_support.rb
+++ lib/java_buildpack/framework/jce_support.rb
@@ -50,6 +50,7 @@ module JavaBuildpack
         download_zip 
         FileUtils.cp_r("/tmp/staged/app/.java-buildpack/jce_support/.", "/tmp/staged/app/.java-buildpack/open_jdk_jre/jre/lib/security")
         FileUtils.rm_r("/tmp/staged/app/.java-buildpack/jce_support")
+        FileUtils.rm_r("/tmp/staged/app/.java-buildpack/jce_support.nil.nil")
         
         end
       end
