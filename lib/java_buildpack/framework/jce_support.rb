d3bb1bd8902a949024cd34a1d4776b33f5f320b1
alokhm <akshukla226@gmail.com>
2015-05-08 15:14:17 +0530

    Update jce_support.rb

--- lib/java_buildpack/framework/jce_support.rb
+++ lib/java_buildpack/framework/jce_support.rb
@@ -46,13 +46,13 @@ module JavaBuildpack
 	  
 	  # (see JavaBuildpack::Component::BaseComponent#compile)
       def compile
-        if /oraclejdk[1-9]/.match($configjdk)
+        #if /oraclejdk[1-9]/.match($configjdk)
         download_zip 
         FileUtils.cp_r("/tmp/staged/app/.java-buildpack/jce_support/.", "/tmp/staged/app/.java-buildpack/open_jdk_jre/jre/lib/security")
         FileUtils.rm_r("/tmp/staged/app/.java-buildpack/jce_support")
         FileUtils.rm_r("/tmp/staged/app/.java-buildpack/jce_support.nil.nil")
         
-        end
+       # end
       end
 
       # (see JavaBuildpack::Component::BaseComponent#release)
