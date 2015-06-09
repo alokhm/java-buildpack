b82274d6898d15db7d87e1919cea561cb3f2f0ff
alokhm <akshukla226@gmail.com>
2015-06-09 19:55:09 +0530

    Update tomcat_instance.rb

--- lib/java_buildpack/container/tomcat/tomcat_instance.rb
+++ lib/java_buildpack/container/tomcat/tomcat_instance.rb
@@ -143,9 +143,9 @@ module JavaBuildpack
           @droplet.copy_resources
           configure_linking
           configure_jasper
-          # unless ENV["valve1"].nil? && ENV["valve2"].nil?
+          unless $value.nil?
           valve_appender
-        #end
+          end
         end
       end
 
