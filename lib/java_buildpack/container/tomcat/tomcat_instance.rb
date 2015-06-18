78759beec51dcbb2931802aa909ee3e8369e3163
alokhm <akshukla226@gmail.com>
2015-06-18 11:27:53 +0530

    Update tomcat_instance.rb

--- lib/java_buildpack/container/tomcat/tomcat_instance.rb
+++ lib/java_buildpack/container/tomcat/tomcat_instance.rb
@@ -233,7 +233,7 @@ module JavaBuildpack
           #context   = REXML::XPath.match(document, '/Server/Service/Engine/Host/Context').first
           #puts document
           if obj.has_key?("valve")
-           
+          unless obj['valve'].has_key?('host')
           for i in 0..obj['valve']['host'].length-1
             valve = REXML::Element.new('Valve')  
             obj['valve']['host'][i].each do |key, array|
@@ -241,7 +241,7 @@ module JavaBuildpack
             end
             host.elements.add(valve)
           end
-          
+        end
            for i in 0..obj['valve']['engine'].length-1
             valve = REXML::Element.new('Valve')  
             obj['valve']['engine'][i].each do |key, array|
